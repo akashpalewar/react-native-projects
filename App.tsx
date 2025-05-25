e9cc8ba2238b02ef8b521eb47dc2ff0070035671
akashpallewar <apalewar@gmail.com>
2025-05-25 19:33:34 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
+import FancyCards from './components/FancyCards';
+import ActionCard from './components/ActionCard';
+import ContactList from './components/ContactList';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+        <ElevatedCards />
+        <FancyCards />
+        <ActionCard />
+        <ContactList />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
